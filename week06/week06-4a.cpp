1e45cb43626cbc86f808d47cd8a7d6c8fa193351
14751043 <14751043@me.mcu.edu.tw>
2026-03-30 11:59:36 +0800

    week06

---
+++ week06/week06-4a.cpp
@@ -0,0 +1,8 @@
+//week06-4a.cpp SOIT106_BASE_001
+#include <stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	printf("%d %d\n",n/7,n%7);
+}
---
+++ week06/week06-5a.cpp
@@ -0,0 +1,14 @@
+//week06-5a.cpp SOIT_107_BASE_0020
+#include <stdio.h>
+int main()
+{
+
+	int N;
+	for(int i=0; i<6; i++) {
+	scanf("%d",&N);
+	printf("%d\n", N*N*N);
+	}
+
+}
+
+
